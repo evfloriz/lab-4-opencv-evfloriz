9b8649f40e7945a42529673ced313db67811bdc6
evfloriz <ev.floriz@gmail.com>
2022-04-06 21:56:09 -0600

    Add main node

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,8 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
-
+dt-exec roslaunch my_package main_node.launch
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
